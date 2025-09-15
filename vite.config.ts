669b70546aa1e6a5139bf3e95a7056268a544d50
Somit <167099702+PrinceZuko10@users.noreply.github.com>
2025-09-16 02:20:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Main_PF_10/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
 
